1b5a7e49964d99923a98997bd2a75889be2ee24b
Raphael Onofre <raphael.onofre@gmail.com>
2018-12-28 22:20:52 -0300

    Make routing works between app and search page

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import React from 'react'
 import './App.css'
 import Bookshelf from "./Components/Bookshelf";
 import Search from "./Components/Search";
+import { Route, Link } from 'react-router-dom';
 
 class BooksApp extends React.Component {
     state = {
-        /**
-         * TODO: Instead of using this state variable to keep track of which page
-         * we're on, use the URL in the browser's address bar. This will ensure that
-         * users can use the browser's back and forward buttons to navigate between
-         * pages, as well as provide a good URL they can bookmark and share.
-         */
-        showSearchPage: false
+
     }
 
     render() {
         return (
             <div className="app">
-                {this.state.showSearchPage ? (
-                    <Search/>
-                ) : (
+                <Route exact path='/' render={() => (
                     <div className="list-books">
                         <div className="list-books-title">
                             <h1>MyReads</h1>
@@ -32,11 +25,16 @@ class BooksApp extends React.Component {
                                 <Bookshelf category='Read'/>
                             </div>
                         </div>
-                        <div className="open-search">
-                            <button onClick={() => this.setState({showSearchPage: true})}>Add a book</button>
-                        </div>
                     </div>
-                )}
+                )} />
+                <Route path='/search' component={Search} />
+                <div className="open-search">
+                    <Link to='/search' className='open-search'>
+                        <button>Add a book</button>
+                    </Link>
+                    {/*<button onClick={() => this.setState({showSearchPage: true})}>Add a book</button>*/}
+                </div>
+
             </div>
         )
     }
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,23 +1,16 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Search extends Component {
     render() {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <button className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close
-                    </button>
+                    <Link to='/' className='close-search'>
+                        Close
+                    </Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                          NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                          You can find these search terms here:
-                          https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                          However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                          you don't find a specific author or title. Every search is limited by search terms.
-                        */}
                         <input type="text" placeholder="Search by title or author"/>
-
                     </div>
                 </div>
                 <div className="search-books-results">
@@ -29,3 +22,11 @@ class Search extends Component {
 }
 
 export default Search;
+
+/*
+  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+  You can find these search terms here:
+  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+  you don't find a specific author or title. Every search is limited by search terms.*/
