3c40c489fabdf5b53546b170514dbb17e49a92fb
Raphael Onofre <raphael.onofre@gmail.com>
2019-01-15 17:01:03 -0300

    Remove basename from index js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}>
+ReactDOM.render(<BrowserRouter>
                     <App />
                 </BrowserRouter>, document.getElementById('root'))
