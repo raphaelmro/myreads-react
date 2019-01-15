ee777fd04f53d6c0b6b4ed063d5954b7531b6fa3
Raphael Onofre <raphael.onofre@gmail.com>
2019-01-15 15:57:40 -0300

    Updates

---
+++ precache-manifest.23dcb719526fbe7cbdb1e550c1f3ba91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads-react/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads-react/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads-react/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3923c7c42fa552ff5f3a",
+    "url": "/myreads-react/static/js/runtime~main.3923c7c4.js"
+  },
+  {
+    "revision": "8218b7c5653149d2312f",
+    "url": "/myreads-react/static/js/main.8218b7c5.chunk.js"
+  },
+  {
+    "revision": "08608d1f9443afdfe1e7",
+    "url": "/myreads-react/static/js/1.08608d1f.chunk.js"
+  },
+  {
+    "revision": "8218b7c5653149d2312f",
+    "url": "/myreads-react/static/css/main.d902aa8e.chunk.css"
+  },
+  {
+    "revision": "08608d1f9443afdfe1e7",
+    "url": "/myreads-react/static/css/1.6640775d.chunk.css"
+  },
+  {
+    "revision": "466a862d754ad7a4c36e737bbc4d2270",
+    "url": "/myreads-react/index.html"
+  }
+];
